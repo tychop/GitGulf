a187c1822559454c45ee28f2da7c47234ce03d9a
Tycho Pandelaar <tycho.pandelaar@klm.com>
2023-10-09 17:03:53 +0200

    Fix

--- src/repository_manager.py
+++ src/repository_manager.py
@@ -3,8 +3,10 @@ import sys
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from src.repository import GitRepo
-from src.table import Table, TableRenderer, Cell, ANSI_BOLD_BRIGHT_WHITE, ANSI_WHITE, ANSI_RED, ANSI_GREEN, ANSI_CYAN,
-											ANSI_PURPLE, ANSI_GREY
+from src.table import (
+    Table, TableRenderer, Cell, ANSI_BOLD_BRIGHT_WHITE, ANSI_WHITE,
+    ANSI_RED, ANSI_GREEN, ANSI_CYAN, ANSI_PURPLE, ANSI_GREY
+)
 
 
 class GitRepoManager:
